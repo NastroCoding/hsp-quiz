a015bb349efc73d753f7a654f9b576c31a4bd87e
NastroCoding <naufalaqilnasrullah0502@gmail.com>
2024-03-26 13:53:00 +0700

    Merge branch 'master' of https://github.com/NastroCoding/hsp-quiz

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,139 +1,117 @@
 @extends('layouts.admin')
 @section('container')
-
-      <!-- Content Header (Page header) -->
-      <section class="content-header">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
         <div class="container-fluid">
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1>Users</h1>
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Users</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="admin.html">Home</a></li>
+                        <li class="breadcrumb-item active">Users</li>
+                    </ol>
+                </div>
             </div>
-            <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="/admin/dashboard">Home</a></li>
-                <li class="breadcrumb-item active">Users</li>
-              </ol>
-            </div>
-          </div>
         </div>
         <!-- /.container-fluid -->
-      </section>
+    </section>
 
-      <!-- Main content -->
-      <div class="card">
+    <!-- Main content -->
+    <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Registered User</h3>
+            <h3 class="card-title">Registered User</h3>
 
-          <div class="card-tools">
-            <div class="input-group input-group-sm" style="width: 150px">
-              <input type="text" name="table_search" class="form-control float-right" placeholder="Search" />
+            <div class="card-tools">
+                <div class="input-group input-group-sm" style="width: 150px">
+                    <input type="text" name="table_search" class="form-control float-right" placeholder="Search" />
 
-              <div class="input-group-append">
-                <button type="submit" class="btn btn-default">
-                  <i class="fas fa-search"></i>
-                </button>
-              </div>
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-default">
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
         <!-- /.card-header -->
         <div class="card-body table-responsive p-0">
-          <table class="table table-hover text-nowrap">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Password</th>
-                <th>Actions</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>183</td>
-                <td>John Doe</td>
-                <td>11-7-2014</td>
-                <td>
-                  <a class="btn btn-block btn-info btn-sm" href="edit-user.html">
-                    Edit
-                  </a>
-                </td>
-                <td>
-                  <button type="button" class="btn btn-block btn-danger btn-sm" data-toggle="modal" data-target="#modal-default">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>219</td>
-                <td>Alexander Pierce</td>
-                <td>11-7-2014</td>
-                <td>
-                  <a class="btn btn-block btn-info btn-sm" href="edit-user.html">
-                    Edit
-                  </a>
-                </td>
-                <td>
-                  <button type="button" class="btn btn-block btn-danger btn-sm" data-toggle="modal" data-target="#modal-default">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>657</td>
-                <td>Bob Doe</td>
-                <td>11-7-2014</td>
-                <td>
-                  <a class="btn btn-block btn-info btn-sm" href="edit-user.html">
-                    Edit
-                  </a>
-                </td>
-                <td>
-                  <button type="button" class="btn btn-block btn-danger btn-sm" data-toggle="modal" data-target="#modal-default">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>175</td>
-                <td>Mike Doe</td>
-                <td>11-7-2014</td>
-                <td>
-                  <a class="btn btn-block btn-info btn-sm" href="edit-user.html">
-                    Edit
-                  </a>
-                </td>
-                <td>
-                  <button type="button" class="btn btn-block btn-danger btn-sm" data-toggle="modal" data-target="#modal-default">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+            <table class="table table-hover text-nowrap">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Actions</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>183</td>
+                        <td>John Doe</td>
+                        <td>11-7-2014</td>
+                        <td>
+                            <a class="btn btn-block btn-info btn-sm" href="edit-user.html">
+                                Edit
+                            </a>
+                        </td>
+                        <td>
+                            <button type="button" class="btn btn-block btn-danger btn-sm">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>219</td>
+                        <td>Alexander Pierce</td>
+                        <td>11-7-2014</td>
+                        <td>
+                            <a class="btn btn-block btn-info btn-sm" href="edit-user.html">
+                                Edit
+                            </a>
+                        </td>
+                        <td>
+                            <button type="button" class="btn btn-block btn-danger btn-sm">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>657</td>
+                        <td>Bob Doe</td>
+                        <td>11-7-2014</td>
+                        <td>
+                            <a class="btn btn-block btn-info btn-sm" href="edit-user.html">
+                                Edit
+                            </a>
+                        </td>
+                        <td>
+                            <button type="button" class="btn btn-block btn-danger btn-sm">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>175</td>
+                        <td>Mike Doe</td>
+                        <td>11-7-2014</td>
+                        <td>
+                            <a class="btn btn-block btn-info btn-sm" href="edit-user.html">
+                                Edit
+                            </a>
+                        </td>
+                        <td>
+                            <button type="button" class="btn btn-block btn-danger btn-sm">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
         <!-- /.card-body -->
-        <div class="modal fade" id="modal-default">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h4 class="modal-title">Delete</h4>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <p>Are you sure?</p>
-            </div>
-            <div class="modal-footer justify-content-between">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-danger">Delete</button>
-            </div>
-          </div>
-          <!-- /.modal-content -->
-        </div>
-        <!-- /.modal-dialog -->
-      </div>
-      </div>
-      <!-- /.content -->    
+    </div>
+    <!-- /.content -->
 @endsection
