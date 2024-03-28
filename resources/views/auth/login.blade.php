69ff49cb868fdccb9e1430feb1c6cff6a885526b
Thekingdx <fakhriprogamers@gmail.com>
2024-03-28 10:43:27 +0700

    login-ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.main')
 
-@section('login')
 
 @if ($errors->any())
 @foreach ($errors->all() as $error)
@@ -16,24 +15,37 @@
 </div>
 @endif
 
-<div class="login-form mt-5">
-    <h1>Login Page</h1>
-    <form action="/signin" method="POST">
-        @csrf
-        <div class="row mb-3">
-            <label for="inputEmail3" class="col-sm-2 col-form-label">Token</label>
-            <div class="col-sm-10">
-                <input type="text" name="token" class="form-control" id="inputEmail3">
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <p href="" class="h1"><b>HSP</b>net</p>
             </div>
-        </div>
-        <div class="row mb-3">
-            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-            <div class="col-sm-10">
-                <input type="password" name="password" class="form-control" id="inputPassword3">
+            <div class="card-body">
+                <p class="login-box-msg">Sign in to start your session</p>
+                <form action="/signin" method="POST">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Token" name="token" id="inputEmail3">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" placeholder="Password" name="password">
+                    </div>
+                    <div class="row">
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block floart-right">Sign In</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+                <p class="mb-0">
+                    <a href="/register" class="text-center">Register</a>
+                </p>
             </div>
+            <!-- /.card-body -->
         </div>
-        <button type="submit" class="btn btn-primary">Sign in</button>
-    </form>
-</div>
-
-@endsection
+        <!-- /.card -->
+    </div>
+</body>
