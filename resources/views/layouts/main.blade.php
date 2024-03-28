81d7893c77c982f1c45dc3ec1eb1687619a45adc
NastroCoding <naufalaqilnasrullah0502@gmail.com>
2024-03-28 13:55:31 +0700

    few-changes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,9 +14,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback" />
     <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css" />
+    <link rel="stylesheet" href="{{ URL::asset('plugins/fontawesome-free/css/all.min.css') }}" />
+    <!-- IonIcons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
     <!-- Theme style -->
-    <link rel="stylesheet" href="../../dist/css/adminlte.min.css" />
+    <link rel="stylesheet" href="{{ URL::asset('dist/css/adminlte.min.css') }}" />
+    {{-- Animate.css --}}
+    <link rel="stylesheet" href="{{ URL::asset('dist/css/animate.css')}}">
 </head>
 
 <body class="hold-transition layout-top-nav">
@@ -92,13 +96,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- REQUIRED SCRIPTS -->
 
     <!-- jQuery -->
-    <script src="../../plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="../../dist/js/adminlte.min.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="../../dist/js/demo.js"></script>
+    <script src="{{ URL::asset('plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap -->
+    <script src="{{ URL::asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- AdminLTE -->
+    <script src="{{ URL::asset('dist/js/adminlte.js') }}"></script>
+
+    {{-- CUSTOM SCRIPT --}}
+    <script src="{{ URL::asset('/dist/js/hspsite.js') }}"></script>
+
+    {{-- JQuery  --}}
+    <script src="{{ URL::asset('/dist/js/jquery.min.js')}}"></script>
+
+    @yield('scripts')
 </body>
 
 </html>
