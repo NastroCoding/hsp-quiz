0e89410e8ccec8681fdfc6f7e89a2e50ccdd0b03
Thekingdx <fakhriprogamers@gmail.com>
2024-04-04 08:28:25 +0700

    sidebar-update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -59,7 +59,7 @@
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <aside class="main-sidebar sidebar-light-primary elevation-4">
             <!-- Brand Logo -->
             <a href="/admin/dashboard" class="brand-link">
                 <img src="{{ URL::asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: 0.8" />
