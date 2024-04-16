f7d3d0519d0351740e7903dbeb43b7a4412fb0bd
NastroCoding <naufalaqilnasrullah0502@gmail.com>
2024-04-16 09:20:19 +0700

    essay-fix

--- resources/views/views/quiz_page.blade.php
+++ resources/views/views/quiz_page.blade.php
@@ -15,72 +15,77 @@
                     <!-- Multiple Choice Form -->
                     @foreach ($questions as $que)
                         @if ($que->number == $question_number)
-                            <div class="card card-default" style="max-width: 500px;">
-                                <!-- Limiting the width to 500px -->
-                                <div class="card-header">
-                                    <h3 class="card-title">{{ $que->question_type }}</h3>
-                                    <h3 class="card-title text-muted float-right">Time Remaining: <span
-                                            id="countdownTimer2"></span></h3>
-                                </div>
-                                <!-- /.card-header -->
-                                <!-- form start -->
-                                <form>
-                                    <div class="card-body">
-                                        <div class="form-group">
-                                            <p>{{ $que->question }}</p>
-                                        </div>
-                                        @foreach ($que->choices as $index => $choice)
+                            @if ($que->question_type == 'multiple_choice')
+                                <div class="card card-default" style="max-width: 500px;">
+                                    <!-- Limiting the width to 500px -->
+                                    <div class="card-header">
+                                        <h3 class="card-title">{{ $que->question_type }}</h3>
+                                        <h3 class="card-title text-muted float-right">Time Remaining: <span
+                                                id="countdownTimer2"></span></h3>
+                                    </div>
+                                    <!-- /.card-header -->
+                                    <!-- form start -->
+                                    <form>
+                                        <div class="card-body">
                                             <div class="form-group">
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="radio" name="question1"
-                                                        id="radio{{ $index + 1 }}">
-                                                    <label class="form-check-label"
-                                                        for="radio{{ $index + 1 }}">{{ $choice->choice }}</label>
-                                                </div>
+                                                <p>{{ $que->question }}</p>
                                             </div>
-                                        @endforeach
-                                        <p class="text-sm text-muted float-right">{{ $que->question_type }}</p>
+                                            @foreach ($que->choices as $index => $choice)
+                                                <div class="form-group">
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="question1"
+                                                            id="radio{{ $index + 1 }}">
+                                                        <label class="form-check-label"
+                                                            for="radio{{ $index + 1 }}">{{ $choice->choice }}</label>
+                                                    </div>
+                                                </div>
+                                            @endforeach
+                                            <p class="text-sm text-muted float-right">{{ $que->question_type }}</p>
+                                        </div>
+                                        <!-- /.card-body -->
+                                        <div class="card-footer">
+                                            <a type="submit" class="btn btn-default"><i class="fas fa-angle-left"></i>
+                                                Back</a>
+                                            <a type="submit" class="btn btn-primary float-right">Next <i
+                                                    class="fas fa-angle-right"></i></a>
+                                        </div>
+                                    </form>
+                                </div>
+                            @elseif ($que->question_type == 'essay')
+                                <div class="card card-default" style="max-width: 500px;">
+                                    <!-- Limiting the width to 500px -->
+                                    <div class="card-header">
+                                        <h3 class="card-title">Question 1</h3>
+                                        <h3 class="card-title text-muted float-right">Time Remaining: <span
+                                                id="countdownTimer3"></span>
+                                        </h3>
                                     </div>
-                                    <!-- /.card-body -->
-                                    <div class="card-footer">
-                                        <a type="submit" class="btn btn-default"><i class="fas fa-angle-left"></i> Back</a>
-                                        <a type="submit" class="btn btn-primary float-right">Next <i
-                                                class="fas fa-angle-right"></i></a>
-                                    </div>
-                                </form>
-                            </div>
+                                    <!-- /.card-header -->
+                                    <!-- form start -->
+                                    <form>
+                                        <div class="card-body">
+                                            <div class="form-group">
+                                                <p>{{ $que->question }}</p>
+                                            </div>
+                                            <div class="form-group">
+                                                <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                                            </div>
+                                            <p class="text-sm text-muted float-right">{{ $que->question_type }}</p>
+                                        </div>
+                                        <!-- /.card-body -->
+                                        <div class="card-footer">
+                                            <a type="submit" class="btn btn-default"><i class="fas fa-angle-left"></i>
+                                                Back</a>
+                                            <a type="submit" class="btn btn-primary float-right">Next <i
+                                                    class="fas fa-angle-right"></i></a>
+                                        </div>
+                                    </form>
+                                </div>
+                            @endif
                         @endif
                     @endforeach
                     <!-- Essay Form -->
-                        <div class="card card-default" style="max-width: 500px;">
-                            <!-- Limiting the width to 500px -->
-                            <div class="card-header">
-                                <h3 class="card-title">Question 1</h3>
-                                <h3 class="card-title text-muted float-right">Time Remaining: <span id="countdownTimer3"></span>
-                                </h3>
-                            </div>
-                            <!-- /.card-header -->
-                            <!-- form start -->
-                            <form>
-                                <div class="card-body">
-                                    <div class="form-group">
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ea at,
-                                            consequatur earum iure ab totam quibusdam corporis quasi, magni voluptatem quis
-                                            aperiam aut maiores doloribus! Earum molestiae odio et!</p>
-                                    </div>
-                                    <div class="form-group">
-                                        <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
-                                    </div>
-                                    <p class="text-sm text-muted float-right">Essay</p>
-                                </div>
-                                <!-- /.card-body -->
-                                <div class="card-footer">
-                                    <a type="submit" class="btn btn-default"><i class="fas fa-angle-left"></i> Back</a>
-                                    <a type="submit" class="btn btn-primary float-right">Next <i
-                                            class="fas fa-angle-right"></i></a>
-                                </div>
-                            </form>
-                        </div>
+
                     <!-- /.col-md-6 -->
                 </div>
                 <!-- /.row -->
