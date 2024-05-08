210434dbeceb318befbce6b460e1971804ed7f77
dinda <dindaamelya105@gmail.com>
2024-05-08 17:15:13 +0700

    Merge branch 'backend' of https://github.com/NastroCoding/hsp-quiz into backend

--- resources/views/admin/quiz/question.blade.php
+++ resources/views/admin/quiz/question.blade.php
@@ -82,7 +82,7 @@
                         <div class="card-footer">
                             <button type="submit" class="btn btn-sm btn-info" data-toggle="modal"
                                 data-id="{{ $question->id }}"
-                                data-target="#edit-{{ $question->question_type }}">Edit</button>
+                                data-target="#edit-{{ $question->question_type }}{{ $question->id }}">Edit</button>
                             <!-- edit quiz modal -->
                             <div class="modal fade" id="edit-question{{ $question->id }}">
                                 <div class="modal-dialog">
@@ -176,6 +176,8 @@
                     <form action="/admin/quiz/question/create" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="card-body">
+                            <!-- Hidden input for quiz ID -->
+                            <input type="hidden" name="quiz_id" value="{{ $quiz->id }}">
                             <div class="form-group">
                                 <label for="inputQuestion">Question</label>
                                 <br>
@@ -193,44 +195,45 @@
                                 <textarea id="inputQuestion" name="question" class="form-control" rows="4"></textarea>
                             </div>
                             <input type="hidden" name="question_type" value="multiple_choice">
-                            <input type="hidden" name="quiz_id" value="{{ $quiz->id }}" />
-                            <input type="hidden" name="number" value="{{ $lastQuestionNumber }}">
+                            <!-- You can include other fields here -->
                             <label>Options</label>
                             <div id="optionsContainer" class="form-group">
                                 <!-- Options will be added dynamically here -->
                                 <!-- Your dynamic options HTML -->
+                                <!-- Initial option -->
                                 <div class="input-group">
+                                    <!-- Ensure that the hidden input has a default value of 0 -->
+                                    <input type="hidden" name="is_correct[]" value="0">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text">
-                                            <input type="checkbox" name="is_correct[]" />
+                                            <!-- Checkbox to mark if the option is correct -->
+                                            <input type="checkbox" name="is_correct_checkbox[]" onchange="updateIsCorrectValue(this)">
                                         </span>
                                     </div>
-                                    <input type="text" class="form-control" placeholder="Option 1"
-                                        name="choices[]" />
-                                    <!-- Modified: added file input for each choice -->
+                                    <!-- Text input for the choice -->
+                                    <input type="text" class="form-control" placeholder="Option 1" name="choices[]">
+                                    <!-- File input for image -->
                                     <div class="input-group-append">
-                                        <span class="input-group-text btn btn-default" style="cursor: pointer"
-                                            onclick="uploadOptionImage(this)">
+                                        <span class="input-group-text btn btn-default" style="cursor: pointer" onclick="uploadOptionImage(this)">
                                             <i class="fas fa-image"></i>
                                         </span>
-                                        <span class="input-group-text btn btn-danger" style="cursor: pointer"
-                                            onclick="removeOption(this)">
+                                        <!-- Button to remove the option -->
+                                        <span class="input-group-text btn btn-danger" style="cursor: pointer" onclick="removeOption(this)">
                                             <i class="fas fa-trash"></i>
                                         </span>
                                     </div>
-                                    <input type="file" accept="image/*" style="display: none"
-                                        onchange="previewOptionImage(event, 'imagePreview1')" class="optionImageInput"
-                                        name="choice_images[]" />
+                                    <!-- Input for image upload -->
+                                    <input type="file" accept="image/*" style="display: none" onchange="previewOptionImage(event, 'imagePreview1')" class="optionImageInput" name="choice_images[]">
                                 </div>
                                 <!-- Image preview container -->
                                 <div class="optionImagePreview" id="imagePreview1"></div>
                             </div>
-                            <div class="form-group">
-                                <br>
-                                <button type="button" class="btn btn-primary" id="addOptionBtn">Add Option</button>
-                                <input type="number" name="point_value" class="form-control float-right"
-                                    placeholder="Points" min="0" style="width: 100px;" required>
-                            </div>
+                        </div>
+                        <div class="form-group">
+                            <br>
+                            <button type="button" class="btn btn-primary" id="addOptionBtn" onclick="addOption()">Add Option</button>
+                            <input type="number" name="point_value" class="form-control float-right"
+                                placeholder="Points" min="0" style="width: 100px;" required>
                         </div>
                         <div class="modal-footer justify-content-between">
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -239,69 +242,64 @@
                         </div>
                     </form>
                 </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+
+
+    <!-- Essay modal -->
+    <div class="modal fade" id="edit-essay">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Essay</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="/admin/quiz/question/create/essay" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <input type="hidden" name="question_type" value="essay">
+                        <input type="hidden" name="quiz_id" value="{{ $quiz->id }}" />
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="inputName">Question</label>
+                                <br>
+                                <small class="float-right text-muted"><span class="text-danger">*</span>Optional</small>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="exampleInputFile2"
+                                            accept="image/*" onchange="previewEssayImage(event, 'fileLabel2')"
+                                            name="essayImage">
+                                        <label class="custom-file-label" id="fileLabel2" for="exampleInputFile2">Choose
+                                            image</label>
+                                        <div class="input-group-append">
+                                            <small class="text-muted float-right input-group-text"><span
+                                                    class="text-danger">*</span>Optional</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="essayImagePreview" class="mt-1"></div>
+                                <!-- Essay image preview container -->
+                                <textarea id="inputDescription" name="question" class="form-control" rows="4"></textarea>
+                            </div>
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-success float-right" id="createQuestionBtn">
+                                Create
+                            </button>
+                        </div>
+                    </form>
+                </div>
                 <!-- /.modal-content -->
             </div>
             <!-- /.modal-dialog -->
         </div>
     </div>
-
-                <!-- Essay modal -->
-                <div class="modal fade" id="edit-essay">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h4 class="modal-title">Essay</h4>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <form action="/admin/quiz/question/create/essay" method="POST"
-                                    enctype="multipart/form-data">
-                                    @csrf
-                                    <input type="hidden" name="question_type" value="essay">
-                                    <input type="hidden" name="quiz_id" value="{{ $quiz->id }}" />
-                                    <div class="card-body">
-                                        <div class="form-group">
-                                            <label for="inputName">Question</label>
-                                            <br>
-                                            <small class="float-right text-muted"><span
-                                                    class="text-danger">*</span>Optional</small>
-                                            <div class="input-group">
-                                                <div class="custom-file">
-                                                    <input type="file" class="custom-file-input"
-                                                        id="exampleInputFile2" accept="image/*"
-                                                        onchange="previewEssayImage(event, 'fileLabel2')"
-                                                        name="essayImage">
-                                                    <label class="custom-file-label" id="fileLabel2"
-                                                        for="exampleInputFile2">Choose
-                                                        image</label>
-                                                    <div class="input-group-append">
-                                                        <small class="text-muted float-right input-group-text"><span
-                                                                class="text-danger">*</span>Optional</small>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div id="essayImagePreview" class="mt-1"></div>
-                                            <!-- Essay image preview container -->
-                                            <textarea id="inputDescription" name="question" class="form-control" rows="4"></textarea>
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer justify-content-between">
-                                        <button type="button" class="btn btn-default"
-                                            data-dismiss="modal">Close</button>
-                                        <button type="submit" class="btn btn-success float-right"
-                                            id="createQuestionBtn">
-                                            Create
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                            <!-- /.modal-content -->
-                        </div>
-                        <!-- /.modal-dialog -->
-                    </div>
-                </div>
 
     <!-- Weighted Multiple Choice modal -->
     <div class="modal fade" id="weighted-mc">
@@ -381,7 +379,7 @@
     @foreach ($questions as $question)
         <!-- Edit modal -->
         @if ($question->question_type == 'multiple_choice')
-            <div class="modal fade" id="edit-{{ $question->question_type }}">
+            <div class="modal fade" id="edit-{{ $question->question_type }}{{ $question->id }}">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -403,10 +401,10 @@
                                                 class="text-danger">*</span>Optional</small>
                                         <div class="input-group">
                                             <div class="custom-file">
-                                                <input type="file" class="custom-file-input" id="exampleInputFile1"
+                                                <input type="file" class="custom-file-input" id="exampleInputFile4"
                                                     accept="image/*" name="questionImage"
-                                                    onchange="previewImage(event, 'question', 'fileLabel1')">
-                                                <label class="custom-file-label" id="fileLabel1" for="exampleInputFile1">
+                                                    onchange="previewImage(event, 'question', 'fileLabel4')">
+                                                <label class="custom-file-label" id="fileLabel4" for="exampleInputFile4">
                                                     @if ($question->images)
                                                         {{ $question->images }}
                                                     @else
