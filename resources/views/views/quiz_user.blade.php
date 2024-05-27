c75d9aa51fa94d17ffc0c7ef8291e1834e375795
Thekingdx <fakhriprogamers@gmail.com>
2024-05-27 16:40:56 +0700

    sort-backend

--- resources/views/views/quiz_user.blade.php
+++ resources/views/views/quiz_user.blade.php
@@ -26,30 +26,34 @@
         <!-- Main content -->
         <div class="content">
             <div class="container">
-                <button class="btn btn-sm btn-default mb-1" data-toggle="modal" data-target="#modal-filter"><i
-                        class="fas fa-sliders"></i></button>
+                <button class="btn btn-sm btn-default mb-1" data-toggle="modal" data-target="#modal-filter">
+                    <i class="fas fa-sliders"></i>
+                </button>
                 <div class="row">
-                    <!-- /.col-md-6 -->
-                    @foreach ($data as $quiz)
-                        <div class="col-lg-4">
-                            <div class="card" style="width: 23rem;">
-                                <img class="card-img-top" src="{{ asset('storage/' . $quiz->thumbnail) }}"
-                                    alt="Card image cap" style="width:100%; height:180px;">
-                                <div class="card-header">
-                                    <h5 class="card-title m-0">{{ $quiz->title }}</h5>
-                                </div>
-                                <div class="card-body">
-                                    <p class="card-text">
-                                        {{ $quiz->description }}
-                                    </p>
-                                    <a class="btn btn-primary" href="/admin/quiz/edit/{{ $quiz->id }}" data-toggle="modal" data-target="#modal-quiz">
-                                        Enter
-                                    </a>
+                    @if ($data->isEmpty())
+                        <div class="col-12">
+                            <p>No quizzes found matching the criteria.</p>
+                        </div>
+                    @else
+                        @foreach ($data as $quiz)
+                            <div class="col-lg-4">
+                                <div class="card" style="width: 23rem;">
+                                    <img class="card-img-top" src="{{ asset('storage/' . $quiz->thumbnail) }}"
+                                        alt="Card image cap" style="width:100%; height:180px;">
+                                    <div class="card-header">
+                                        <h5 class="card-title m-0">{{ $quiz->title }}</h5>
+                                    </div>
+                                    <div class="card-body">
+                                        <p class="card-text">{{ $quiz->description }}</p>
+                                        <a class="btn btn-primary" href="/admin/quiz/edit/{{ $quiz->id }}"
+                                            data-toggle="modal" data-target="#modal-quiz">
+                                            Enter
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    @endforeach
-                    <!-- /.col-md-6 -->
+                        @endforeach
+                    @endif
                 </div>
                 <!-- /.row -->
             </div>
@@ -59,38 +63,36 @@
     </div>
 
     @if ($data->isNotEmpty())
-
-    <div class="modal fade" id="modal-quiz">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">Enter Quiz</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form action="/quiz/view/{{ $quiz->slug }}" method="POST">
-                    @csrf
-                    <div class="modal-body">
-                        <div class="card-body">
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Enter quiz code</label>
-                                <input type="text" name="token" class="form-control" id="exampleInputEmail1"
-                                    placeholder="XXXXXX">
+        <div class="modal fade" id="modal-quiz">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Enter Quiz</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form action="/quiz/view/{{ $quiz->slug }}" method="POST">
+                        @csrf
+                        <div class="modal-body">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Enter quiz code</label>
+                                    <input type="text" name="token" class="form-control" id="exampleInputEmail1"
+                                        placeholder="XXXXXX">
+                                </div>
                             </div>
+                            <!-- /.card-body -->
                         </div>
-                        <!-- /.card-body -->
-                    </div>
-                    <div class="modal-footer justify-content-between">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-success">Go</button>
-                    </div>
-                </form>
+                        <div class="modal-footer justify-content-between">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-success">Go</button>
+                        </div>
+                    </form>
+                </div>
+                <!-- /.modal-content -->
             </div>
-            <!-- /.modal-content -->
+            <!-- /.modal-dialog -->
         </div>
-        <!-- /.modal-dialog -->
-    </div>
-
     @endif
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -75,6 +75,8 @@ Route::controller(QuizController::class)->group(function () {
     Route::get('/admin/quiz/delete/{id}', 'destroy')->middleware('admin');
     Route::get('/quiz/view/{slug}/{number}', 'quiz_num')->middleware('auth');
     Route::post('/quiz/view/{slug}', 'quiz_view')->middleware('auth');
+    Route::get('/quizzes', 'index')->name('quiz.index'); // Add the new route here
+    Route::get('/admin/quizzes', 'adminIndex')->name('quiz.adminIndex'); // Add the new route here
 });
 
 Route::controller(QuestionController::class)->group(function () {
