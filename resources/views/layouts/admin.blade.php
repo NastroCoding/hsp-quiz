7f7ec05b3f69381540686b40bf5e44e41a698a36
NastroCoding <naufalaqilnasrullah0502@gmail.com>
2024-06-27 14:47:54 +0700

    Begone!

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -58,7 +58,7 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="{{ Auth::user()->image }}" class="img-circle elevation-2" alt="User Image" />
+                        <img src="{{ Auth::user()->image ? Auth::user()->image : Storage::url('profile_pictures/user.png') }}" class="img-circle elevation-2" alt="User Image" />
                     </div>
                     <div class="info">
                         <a href="/admin/profile" class="d-block">{{ Auth::user()->name }}</a>
